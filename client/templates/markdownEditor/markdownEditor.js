7b91cc9ed93ee8827faedfcd018cf1e81db3d83e
joshherkness <joshherkness@gmail.com>
2016-02-07 15:24:08 -0500

    Config ace editor

--- client/templates/markdownEditor/markdownEditor.js
+++ client/templates/markdownEditor/markdownEditor.js
@@ -6,10 +6,40 @@ Template.markdownEditor.onRendered( function() {
 
 });
 
-Template.aceEditor.onRendered( function() {
-    window.editor = new ReactiveAce();
+Template.editor.helpers({
+  configEditor: function (editor) {
+      return function(editor) {
+          // Set some options for the Ace editor
+          editor.setTheme('ace/theme/tomorrow');
+          editor.getSession().setMode('ace/mode/markdown');
+          editor.setShowPrintMargin(false);
+          editor.getSession().setUseWrapMode(true);
+          editor.setOptions({
+              highlightActiveLine: false,
+              highlightGutterLine: false,
+              showGutter: true,
+              tabSize: 4,
+              displayIndentGuides: false,
+              wrap: true,
+              showPrintMargin: false,
+              fontFamily: "Source Code Pro",
+              fontSize: "14pt",
+              scrollPastEnd: true,
+          });
+          editor.renderer.setScrollMargin(40, 40);
+          editor.renenrer.setGutterMargin(40, 40);
 
-    editor.attach(ace.edit("aceEditor"));
-    editor.theme = "tomorrow";
-    editor.syntaxMode = "markdown";
+          // Editor events
+          editor.on("change", function () {
+              var div = document.getElementById("editor");
+              console.log(" " + div.style.height);
+          });
+      }
+  }
+});
+
+Template.editor.events({
+    "blur": function (event) {
+        console.log("Hello");
+    }
 });
