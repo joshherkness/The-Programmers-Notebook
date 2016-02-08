2c101ce2ebfe50c22d57963f609725878271e321
joshherkness <joshherkness@gmail.com>
2016-02-07 22:30:49 -0500

    Replace ShareJS-Ace package for standalone Ace package, no need for ShareJS

--- client/templates/markdownEditor/markdownEditor.js
+++ client/templates/markdownEditor/markdownEditor.js
@@ -4,42 +4,27 @@
 
 Template.markdownEditor.onRendered( function() {
 
-});
+    AceEditor.instance("editor",{
+        theme: "github",
+        mode: "markdown"
+    },function(editor){
+        editor.renderer.setScrollMargin(40, 40);
+        editor.setOptions({
+            highlightActiveLine: false,
+            highlightGutterLine: false,
+            showGutter:true,
+            fontSize: "14pt",
+            fontFamily: "Source Code Pro",
+            tabSize: 4,
+            displayIndentGuides: false,
+            showPrintMargin: false,
+            scrollPastEnd: 0.5,
+            wrap: false
+        });
 
-Template.editor.helpers({
-  configEditor: function (editor) {
-      return function(editor) {
-          // Set some options for the Ace editor
-          editor.setTheme('ace/theme/tomorrow');
-          editor.getSession().setMode('ace/mode/markdown');
-          editor.setShowPrintMargin(false);
-          editor.getSession().setUseWrapMode(true);
-          editor.setOptions({
-              highlightActiveLine: false,
-              highlightGutterLine: false,
-              showGutter: true,
-              tabSize: 4,
-              displayIndentGuides: false,
-              wrap: true,
-              showPrintMargin: false,
-              fontFamily: "Source Code Pro",
-              fontSize: "14pt",
-              scrollPastEnd: true,
-          });
-          editor.renderer.setScrollMargin(40, 40);
-          editor.renenrer.setGutterMargin(40, 40);
-
-          // Editor events
-          editor.on("change", function () {
-              var div = document.getElementById("editor");
-              console.log(" " + div.style.height);
-          });
-      }
-  }
-});
-
-Template.editor.events({
-    "blur": function (event) {
-        console.log("Hello");
-    }
+        // Editor events example
+        editor.on("change", function () {
+            console.log(editor.getValue());
+        });
+    });
 });
