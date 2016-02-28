99001e6e29c69e42c47c029210715f82677f121b
tgnass <tgnass@gmail.com>
2016-02-27 21:12:38 -0500

    Editor implementing

--- client/templates/editor/editor.js
+++ client/templates/editor/editor.js
@@ -4,3 +4,45 @@ Template.editor.events({
         console.log("here");
     }
 });
+
+// editor.js
+Template.markdownEditor.onCreated(function () {
+
+    // Variables
+    this.currentMode = new ReactiveVar("text");
+    this.modeList = ace.require('ace/ext/modelist');
+    this.shouldHideMode = new ReactiveVar(false);
+    this.shouldEnableModeSelect = new ReactiveVar(true);
+
+    this.buffer = new Buffer("editor");
+});
+
+Template.markdownEditor.onRendered(function () {
+
+    // Create the ace editor
+    this.editor = ace.edit("editor");
+
+    this.editor.getSession().setMode("ace/mode/" + Template.instance().currentMode.get());
+
+    this.editor.setTheme("ace/theme/tomorrow");
+
+    this.editor.renderer.setScrollMargin(40, 40);
+
+    this.editor.setOptions({
+        highlightActiveLine: false,
+        highlightGutterLine: false,
+        showGutter:true,
+        fontSize: "14pt",
+        fontFamily: "Source Code Pro",
+        tabSize: 4,
+        displayIndentGuides: false,
+        showPrintMargin: false,
+        scrollPastEnd: 0.5,
+        wrap: true
+    });
+
+    this.autorun( function () {
+        this.editor.getSession().setMode("ace/mode/" + this.currentMode.get());
+    }.bind(this));
+
+});
