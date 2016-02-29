fe2a6fe7dfda42cd8aecb81a146cb5d79d06896b
ChuckFinly <npneumeyer@oakland.edu>
2016-02-29 03:54:58 -0500

    Side Bar Adding/Deleting Files
    Just need to get it so they can be accessed via editor

--- client/templates/menu/menu.js
+++ client/templates/menu/menu.js
@@ -1,11 +1,36 @@
 // menu.js
+Files = new Mongo.Collection(null);
 
 Template.menu.helpers({
-
+  files: function () {
+  // Show newest tasks at the top
+  return Files.find({}, {sort: {createdAt: -1}});
+}
 })
 
 Template.menu.events({
+  "submit .new-file": function (event) {
+  // Prevent default browser form submit
+  event.preventDefault();
+  // Get value from form element
+  var text = event.target.text.value;
+  // Insert a task into the collection
+  Files.insert({
+    text: text,
+    data: "",
+    createdAt: new Date() // current time
+  });
+  // Clear form
+  event.target.text.value = "";
+},
     'click .menu-item': function (event) {
         console.log("Click menu item");
     }
+});
+
+Template.file.events({
+
+  "click .delete": function () {
+    Files.remove(this._id);
+  }
 });
